d9fb57e41f9702d2f448f052c776b2f6bb4f9b9f
Carl Sjövall <caersj@utu.fi>
2023-12-18 16:14:03 +0200

    day complete

--- day2.py
+++ day2.py
@@ -18,23 +18,26 @@ def oneGame(game):
     return final
 
 def check(game):
+    bigRed = int()
+    bigBlue = int()
+    bigGreen = int()
     for i in game:
-        if i[1] == "red" and int(i[0]) > 12:
-            return False
-        if i[1] == "green" and int(i[0]) > 13:
-            return False
-        if i[1] == "blue" and int(i[0]) > 14:
-            return False
-        
-    return True
+        if i[1] == "red" and int(i[0]) >= bigRed:
+            bigRed = int(i[0])
+            print(i)
+        if i[1] == "green" and int(i[0]) >= bigGreen:
+            bigGreen = int(i[0])
+            print(i)
+        if i[1] == "blue" and int(i[0]) >= bigBlue:
+            bigBlue = int(i[0])
+            print(i)  
+    return int(bigBlue*bigRed*bigGreen)
 
 
 for i in b:
     c.append(oneGame(i))
 sum = 0
-for i in range(0,len(c)-1):
-    if check(c[i]):
-        print(check(c[i]))
-        sum = sum +i+1
+for i in range(0,len(c)):
+    sum = sum + check(c[i])
 
 print(sum)
