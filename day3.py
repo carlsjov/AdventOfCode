1dc97493e058402868dfc9881d8d0fe108a656c0
Carl Sjövall <caersj@utu.fi>
2023-12-23 00:20:37 +0200

    day3 complete

--- day3.py
+++ day3.py
@@ -28,9 +28,7 @@ def check(m,i,q,s):
                 for l in range(minX,maxX+1):
                     if (j,l) not in k:
                         q2.append((j,l))
-            #t = readNumbers(m,q2,k,temp=[])
-            #if len(t) == 2:
-            #    s = s + t[0]*t[1]
+            s = s + readNumbers(m,q2,k)            
         if x+1 < lenX:
             q.append((y,x+1))
             q.pop(0)
@@ -40,14 +38,47 @@ def check(m,i,q,s):
             return check(m,i,q,s)
     return s
 
-def readNumbers(m,q2,k,temp):
-    if len(q2) != 0:
-        x = q2[0][1]
-        y = q2[0][0]
-        if m[y][x].isdigit():
-            print(m[y][x])
-        q2.pop(0)
-        return readNumbers(m,q2,k,temp)
+def readNumbers(m,q2,k):
+    numbers = []
+    for i in q2:
+        x = i[1]
+        y = i[0]
+        ind = []
+        temp = ""
+        if m[y][x].isdigit() and (y,x) not in k:
+            k.append((y,x))
+            ind.append((y,x))
+            var = True
+            xPlus = x
+            xMinus = x
+            while var:
+                if  xPlus+1 < len(m[y]) and  m[y][xPlus+1].isdigit() and (y,xPlus+1) not in k:
+                    k.append((y,xPlus+1))
+                    ind.append((y,xPlus+1))
+                    xPlus = xPlus+1
+                else:
+                    var = False
+            var = True
+            while var:
+                if  xMinus-1 >= 0 and  m[y][xMinus-1].isdigit() and (y,xMinus-1) not in k:
+                    k.append((y,xMinus-1))
+                    ind.append((y,xMinus-1))
+                    xMinus = xMinus-1
+                else:
+                    var = False
+            ind.sort()
+            for j in ind:
+                temp = temp + str(m[j[0]][j[1]])
+            numbers.append(int(temp))
+    
+    if len(numbers) == 2:
+        sum = numbers[0]*numbers[1]
+    else:
+        sum = 0
+    return sum
+
+
+            
 
 
 q = [(0, 0)]
