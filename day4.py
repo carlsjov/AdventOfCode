b78481bd55e8e315525884b2ff785d0c4a22ab06
Carl Sjövall <caersj@utu.fi>
2023-12-23 02:40:25 +0200

    day4 complete

--- day4.py
+++ day4.py
@@ -18,15 +18,36 @@ for i in range(len(cards)):
     cards[i].pop(0)
     cards[i].pop(0)
 
-s = -1
-sum = 0
+s = 0
+wins = []
 for i in range(len(cards)):
     for j in range(len(cards[i])):
         if cards[i][j] in numbers[i]:
             s = s+1
-    if s != -1:
-        sum = sum + pow(2,s)
-    s = -1
+    wins.append(s)        
+    s = 0
 
+pieces =[]
+for i in range(len(cards)):
+    pieces.append(1)
+
+def checkPieces(p,w,x):
+    if x < len(p):
+        for i in range(0,p[x]):
+            newX = x
+            if newX+1 < len(p):
+                newX = newX + 1
+            for j in range(0,w[x]):
+                p[newX] = p[newX]+1
+                if newX +1 < len(p):
+                    newX = newX +1
+        return checkPieces(p,w,x+1)
+    return p
+
+pieces = checkPieces(pieces,wins,x=0)
+print(wins)
+print(pieces)
+sum = 0
+for i in pieces:
+    sum = sum + i
 print(sum)
-    
